af3256d1aec5ef23b1c8bd9a323dd58e31dacf21
Ibraheem Zaffar <askibbi@yahoo.com>
2025-04-14 23:37:04 +0500

    last-try

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     // other variables...
   },
 };
