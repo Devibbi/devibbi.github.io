69422aaac3fb6e3a87899db7e60ad1fb419ded10
Ibraheem Zaffar <askibbi@yahoo.com>
2025-04-15 22:36:47 +0500

    fixed-variable-issue-to-great-extent3

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   experimental: {
     serverActions: {},
   },
+  reactStrictMode: true,
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
   env: {
     NEXT_PUBLIC_CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
